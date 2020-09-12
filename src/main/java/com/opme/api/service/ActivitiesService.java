8014deac863c14cd2323256b2a6ac662cfe7b443
Paulo Meira <meira.paulo@outlook.com>
2020-09-12 20:30:10 -0300

    Adding Service Class

---
+++ src/main/java/com/opme/api/service/ActivitiesService.java
@@ -0,0 +1,36 @@
+package com.opme.api.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.opme.api.entity.Activities;
+import com.opme.api.repository.ActivitiesRepository;
+
+@Service("activitiesService")
+public class ActivitiesService {
+	
+	@Autowired
+	private ActivitiesRepository actRepo;
+	
+	public JsonObject getAllActivities() {
+		List<Activities> activitiesList = new ArrayList<Activities>();
+		JsonArray jsonList = new JsonArray();
+		JsonObject result = new JsonObject();
+		
+		actRepo.findAll().forEach(activitiesList::add);
+		if(!activitiesList.isEmpty()) {
+			for(Activities registry : activitiesList) {
+				jsonList.add(registry.toJson());
+			}
+			result.add("activities", jsonList);
+		}else {
+			result = null;
+		}
+		return result;
+	}
+}
